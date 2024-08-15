fa06f581afc3c76d0c60425fc203d5dabf2e0dba
Nicooooooooo572 <nicolas.legout57@orange.fr>
2024-08-15 20:16:22 +0200

    pipe et commentaires

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -3,47 +3,42 @@ extends Node2D
 @onready var background = $Background
 @onready var camera = $Camera2D
 
-var zoom
+var zoom_x : int
 var speed : float
 const START_SPEED : float = 0.5
 const CAM_START_POS := Vector2i(0, 0)
 var screen_size : Vector2i
-var used_rect
-var cell_size
-var length_background
+var used_rect #jsp
+var cell_size : Vector2i
+var length_background : int
 
 
-# Called when tgame()he node enters the scene tree for the first time.
+
 func _ready():
-	#Obtient taille de l'écran
+
+	#Obtient taille de l'écran et zoom de la camera
 	screen_size = get_window().size
+	zoom_x = camera.get_zoom().x
 	
-	#Obtient le zoom de la cam
-	zoom = camera.get_zoom().x
-	
-	# Obtient le rectangle utilisé par le TileMap
+	# Obtient le nombre de tiles utilisés et leurs tailles. Calcul largeur TileMap
 	used_rect = background.get_child(0).get_used_rect()
-
-	# La taille d'une cellule (Vector2)
 	cell_size = background.get_child(0).get_tileset().get_tile_size()
-
-	# Calculer la largeur en pixels
 	length_background = used_rect.size.x * cell_size.x
 	
-	new_game()
-
-
-func new_game():
+	#Initialise les positions
 	camera.position = CAM_START_POS
 	background.position =  Vector2i(0,0)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(_delta):
+	#Vitesse de départ
 	speed = START_SPEED
 	
 	#Move camera
 	camera.position.x += speed
 	
-	if camera.position.x - background.position.x > (length_background - (screen_size.x / zoom + 3)):
+	#Si la caméra va plus loin que la taille de la TileMap, moins la taille de l'écran,
+	#alors remet à jour la position de la caméra au début.
+	if camera.position.x - background.position.x > (length_background - (screen_size.x / zoom_x + 3)):
 		background.position.x = camera.position.x
