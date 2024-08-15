314ff57f93280fec6ca994fc4e56db9ecb695068
Loyceman <florian.sredniawa@imt-atlantique.net>
2024-08-15 22:43:33 +0200

    fix : point is now bind to a truely timer

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -3,22 +3,25 @@ extends Node2D
 @onready var background = $Background
 @onready var camera = $Camera2D
 @onready var scorelabel : Label = $Camera2D/Control/ScoreLabel
+@onready var timer : Timer = $TimerPoint
 
 var zoom_x : int 
 var speed : float
 const START_SPEED : float = 0.5
-const CAM_START_POS := Vector2i(0, 0)
+const CAM_START_POS : Vector2i = Vector2i(0, 0)
 var screen_size : Vector2i
 var used_rect #jsp
 var cell_size : Vector2i
 var length_background : int
 
-
 var score : float = 0 
+const pointsEveryTimeout : int = 1
 
 signal change_to_game_over(score: int)
 
 func _ready():
+	timer.start()
+	timer.timeout.connect(increase_score)
 	$"Paper Boat".game_over.connect(call_scene_manager_for_game_over)
 	
 	#Obtient taille de l'écran et zoom de la camera
@@ -52,8 +55,6 @@ func _process(delta):
 	#alors remet à jour la position de la caméra au début.
 	if camera.position.x - background.position.x > (length_background - (screen_size.x / zoom_x - 14)):
 		background.position.x = camera.position.x
-		
-	increase_score(100*delta)
 
 # Fonction pour mettre à jour l'affichage du score
 func _update_score():
@@ -61,8 +62,8 @@ func _update_score():
 		scorelabel.set_text("Score: %d" % score)
 
 # Exemple d'une fonction pour augmenter le score
-func increase_score(points: int):
-	score += points
+func increase_score():
+	score += pointsEveryTimeout
 	$"Blockers Factory".calculate_spawn_intervalle(score)
 	$EnnemiesFactory.calculate_spawn_intervalle(score)
 	_update_score()
