8d5e63b5f54bc1407aa17d96ca597f0baf6ac892
Nicooooooooo572 <nicolas.legout57@orange.fr>
2024-08-17 12:01:12 +0200

    fish speed in both direction

--- blockers/fish.gd
+++ blockers/fish.gd
@@ -19,6 +19,9 @@ func _ready():
 	velocity.x = direction * speed
 
 func _physics_process(delta):
-	speed = Global.addSpeed + init_speed
+	if direction == 1:
+		speed = init_speed - Global.addSpeed
+	if direction == -1:
+		speed = Global.addSpeed + init_speed
 	velocity.x = direction * speed
 	move_and_collide(velocity * delta)
